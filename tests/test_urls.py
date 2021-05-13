35c7d3f2888e0373e827ca8e763c5ed883d514d0
Dung Nguyen <contact@dungnt.net>
2021-05-13 20:06:16 +0700

    2 - disable FLoC only

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from django.http import HttpResponse
 
 urlpatterns = [
-    path("", lambda request: HttpResponse("Hello World"), name="index")
+    path("", lambda request: HttpResponse("Hello World"), name="index"),
+    path(
+        "test_exists",
+        lambda request: HttpResponse("Hello World", headers={'Permissions-Policy': 'geolocation=*, camera=()'})
+    )
 ]
--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,10 @@ from django.test import TestCase
 
 
 class MiddlewareTest(TestCase):
-    def test_middleware(self):
+    def test_not_exists_header(self):
         response = self.client.get('/')
+        self.assertEqual(response['Permissions-Policy'], 'interest-cohort=()')
 
-        self.assertEqual(response["Permissions-Policy"], 'interest-cohort=()')
+    def test_exists_header(self):
+        response = self.client.get('/test_exists')
+        self.assertEqual(response['Permissions-Policy'], 'geolocation=*, camera=(), interest-cohort=()')
